fa55a682c03559e127955a038874af218c6330ae
MattChaddock <41082345+MattChaddock@users.noreply.github.com>
2023-12-03 19:37:48 +0000

    Updated cc-template to preferred format, and added Day_4 blank

--- Day_4/tests/test_main.py
+++ Day_4/tests/test_main.py
@@ -1,8 +1,9 @@
-from {{ cookiecutter.project_slug }}.main import execute
+from Day_4.main import execute
 
 
 def test_example():
     assert execute("test_input.txt")
+
 
 def test_main():
     assert execute("input.txt")
